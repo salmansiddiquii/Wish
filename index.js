3047ff62ffd4a3690fedfa67b5767eff5ae742eb
SALMAN SIDDIQUI <131545147+salmansiddiquii@users.noreply.github.com>
2023-09-03 12:53:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,119 @@
+
+/*
+$("h1").css("color", "red");
+$("button").css("backgroundColor", "red")
+*/
+
+
+/*
+
+$(document).ready(function(){
+$("h1").css("color", "red")
+})
+
+
+
+$(document).ready(function(){
+$("button").css("color", "red")
+})
+*/
+
+alert("Hey Sweet heart💞 Welcome!")
+
+//console.log($("h1").css("fontSize"));
+
+
+//$("h1").css("color", "blue")
+//
+//$("button").css("backgroundColor", "red")
+//
+//console.log($("h1").css("fontSize"))
+//
+//
+//$("body").css("backgroundColor", "blue")
+//
+//$("h1").css("color", "yellow")
+//
+//$("button").css("border", "none")
+//
+//
+//$("button").css("padding", "10px")
+//
+//
+//$("button").css("borderRadius", "50px")
+//
+//
+//
+//$("button").css("boxShadow", "10px 10px  black" )
+/*
+
+$("h1").addClass("big-tittle")
+
+
+$("h1").removeClass("big-tittle")
+
+
+$("button").css("border", "none")
+
+$("button").css("backgroundColor", "lightGreen")
+
+$("button").css("padding", "10px")
+
+$("button").css("borderRadius", "50px")
+
+//$("h1").addClass("big-tittle");
+
+$("h1").text("My Name is Salman Siddiqui")
+
+$("h1").html("<em>Salman Siddiqui<em/>")
+
+$("h1").html("<em>Salman Siddiuqi</em>")
+
+
+
+$("button").addClass("box-shadow");
+*/
+
+//box-shadow: 2px 2px 2px rgb(55, 71, 131);
+
+
+
+//$("button").addClass("box-shadow");
+
+/*
+
+let displayValue = '';
+let currentOperator = '';
+
+function appendToDisplay(value) {
+    if (displayValue === 'Error') {
+        clearDisplay();
+    }
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    currentOperator = '';
+    document.getElementById('display').value = displayValue;
+}
+
+function calculateResult() {
+    try {
+        const result = eval(displayValue);
+        document.getElementById('display').value = result;
+        displayValue = result.toString();
+        currentOperator = '';
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+        displayValue = '';
+        currentOperator = '';
+    }
+}
+*/
+
+
+
+
+
